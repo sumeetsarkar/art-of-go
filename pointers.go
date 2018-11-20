4a04808bce44c5c583b32dba906a9b31f43f762d
Sumeet Sarkar <sumeetsarkar4@gmail.com>
2018-11-20 20:15:32 +0530

    pointers update

--- pointers.go
+++ pointers.go
@@ -8,14 +8,18 @@ func main() {
 
 	fmt.Println("value of a", a)
 	fmt.Println("pointer address", p)
-	fmt.Println("value from pointer", *p)
+	fmt.Println("value from pointer *p", *p)
 
 	*p += 1
-	fmt.Println("value from pointer", *p)
+	fmt.Println("value from pointer *p", *p)
 
 	p = nil
 	// *p += 1 // SIGSEGV: Runtime error invalid memory address or nil pointer dereferenc
 
 	var q *int = &a
-	fmt.Println("value from pointer", *q)
+	fmt.Println("value from pointer *q", *q)
+
+	// implicit pointer declaration and assignment using shorthand
+	z := &a
+	fmt.Println("value from pointer *z", *z)
 }
