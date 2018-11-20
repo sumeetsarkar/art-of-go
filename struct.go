4d36d85718a5583c2c3e2b84b1e62bff5653e1e6
Sumeet Sarkar <sumeetsarkar4@gmail.com>
2018-11-21 00:33:49 +0530

    go structs, anonymous structs and fields

--- struct.go
+++ struct.go
@@ -44,4 +44,40 @@ func main() {
 	jessica.age = 28
 	// accessing field values
 	fmt.Printf("%s %s is %d years old\n", jessica.fname, jessica.lname, jessica.age)
+
+	// anonymous structs
+	sumeet := struct {
+		name string
+		age  int
+	}{
+		name: "Sumeet Sarkar",
+		age:  28,
+	}
+	fmt.Println(sumeet)
+
+	// anonymous fields in structs
+	type AnonyFields struct {
+		string
+		int
+		bool
+	}
+	magic := AnonyFields{
+		string: "magic",
+		int:    100,
+		bool:   true,
+	}
+	fmt.Println(magic)
+	fmt.Println(magic.string)
+	magic.int += 100
+	fmt.Println(magic.int)
+
+	// anonymous struct with anonymous fields
+	nextlevelmagic := struct {
+		string
+		int
+		bool
+		// bool	// not allowed, duplicate field bool
+	}{"nextlevelmagic", 100, true}
+	fmt.Println(nextlevelmagic)
+	fmt.Println(nextlevelmagic.bool)
 }
