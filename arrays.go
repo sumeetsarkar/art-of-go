e087c3b61c417c2d257bbaab45550039af4c6e12
Sumeet Sarkar <sumeetsarkar4@gmail.com>
2018-11-21 18:27:04 +0530

    arrays in go

--- arrays.go
+++ arrays.go
@@ -34,4 +34,39 @@ func main() {
 	rgbCopy[1] = "M"
 	rgbCopy[2] = "Y"
 	fmt.Println(rgb, rgbCopy) // [R G B] [C M Y]
+
+	// iterating arrays
+
+	// method 1, for loop using length
+	for i := 0; i < len(rgb); i++ {
+		fmt.Println(rgb[i])
+	}
+
+	// method 2, for loop using range
+	// range returns index and value at the index, for each iteration
+	for i, v := range rgb {
+		fmt.Printf("arr[%d]: %s\n", i, v)
+	}
+
+	// suppress index
+	for _, v := range rgb {
+		fmt.Println(v)
+	}
+
+	// multidimensional array
+	arrMulti := [2][2]string{
+		{},
+	}
+	fmt.Println(arrMulti) // [[0 0] [0 0]]
+
+	// assign array in first dimension
+	arrMulti[0] = [...]string{"some", "value"}
+	arrMulti[1] = [...]string{"another", "value"}
+	fmt.Println(arrMulti) // [[some value] [another value]]
+
+	// assign values specific to position in 2d array
+	arrMulti[0][1] = "value modified"
+	fmt.Println(arrMulti) // [[some value modified] [another value]]
+
+	// Arrays though easy to work with it restricts on its size
 }
