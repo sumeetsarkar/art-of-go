34a0a09e0ed74746d86fc4b94a0bec3829f31fd9
Sumeet Sarkar <sumeetsarkar4@gmail.com>
2018-11-25 02:30:24 +0530

    go interfaces

--- interfaces.go
+++ interfaces.go
@@ -32,8 +32,23 @@ func main() {
 	rect := Rectangle{10, 20}
 	circle := Circle{10}
 
+	// Since both Rectangle and Circle both implement the Shape interface
+	// by implementing all the methods in Shape (here area method)
+	// Rectangle and Cirle both are Shape type objects as well
+
+	// Example 1
+	var s1, s2 Shapes = rect, circle
+	fmt.Println(s1.area())
+	fmt.Println(s2.area())
+
+	// Example 2
 	shapes := []Shapes{rect, circle}
 	for _, v := range shapes {
 		fmt.Println("Area of", reflect.TypeOf(v), "=", v.area())
 	}
+
+	var s3 Shapes
+	fmt.Println(reflect.TypeOf(s1)) // main.Rectangle
+	fmt.Println(reflect.TypeOf(s2)) // main.Circle
+	fmt.Println(reflect.TypeOf(s3)) // <nil>
 }
