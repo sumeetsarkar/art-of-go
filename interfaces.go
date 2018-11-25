c0fe0d68fd61460407341606868efb427bbc4a26
Sumeet Sarkar <sumeetsarkar4@gmail.com>
2018-11-25 14:10:50 +0530

    go interfaces

--- interfaces.go
+++ interfaces.go
@@ -51,4 +51,86 @@ func main() {
 	fmt.Println(reflect.TypeOf(s1)) // main.Rectangle
 	fmt.Println(reflect.TypeOf(s2)) // main.Circle
 	fmt.Println(reflect.TypeOf(s3)) // <nil>
+
+	// Empty Interface
+	// It has zero methods to implement and hence all types implement it
+
+	// example
+
+	logit := func(data interface{}) {
+		fmt.Println(data)
+	}
+	logit(s1)
+	logit(s2)
+	logit(s3)
+
+	// Since, interfaces can accept any type,
+	// Type assertions is needed to deduce the underlying type
+	// This can be done using reflect.TypeOf or else using following
+
+	var data1 interface{} = "string type data"
+	var data2 interface{} = 1000
+
+	// fmt.Println(data1.(int)) // panic: interface conversion: interface {} is string, not int
+	fmt.Println(data2.(int)) // works, sunce data2 is actually int
+
+	// to do type assertions safer way instead
+	// Note: this only works for interface {} type vars
+
+	v, ok := data1.(string)
+	fmt.Println(v, ok)
+	if ok {
+		fmt.Println("data1 is string")
+	}
+
+	// Type switch
+	// <your-interface-var>.(type) can only be used with switch
+
+	switch data2.(type) {
+	case int:
+		fmt.Println("data2 is int, value=", data2.(int))
+	case string:
+		fmt.Println("data2 is string, value=", data2.(string))
+	default:
+		fmt.Println("unkown type")
+	}
+
+	// Example 2 for type assertion
+
+	// create rect1 and circle1 objects
+	rect1 := Rectangle{40, 10}
+	circle1 := Circle{15}
+
+	// declare 2 vars of type Shapes
+	var shape1, shape2 Shapes
+
+	// assign rect1 and circle1 to shape1 and shape2 resp.
+	shape1, shape2 = rect1, circle1
+
+	// declare an anonymous function taking Shape type vars
+	computeArea := func(i interface{}) {
+		// get the actual type
+		switch v := i.(type) {
+		case Shapes:
+			fmt.Println("area of the shape is", v.area())
+		case int:
+			fmt.Println("v is int, value=", v)
+		case string:
+			fmt.Println("v is string, value=", v)
+		default:
+			fmt.Println("unkown type, cannot compute area")
+		}
+	}
+
+	computeArea(shape1)
+	computeArea(rect1) // since rect1 implemente Shape
+	computeArea(shape2)
+	computeArea(circle1) // since circle1 implements Shape
+
+	computeArea(data1)
+	computeArea(data2)
+
+	// not allowed
+	// v := data1.(type)	// use of .(type) outside type switch
+	// fmt.Println(v)
 }
