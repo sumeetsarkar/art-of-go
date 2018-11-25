04e7399bb2964b1e0e2de827b2019edc92970522
Sumeet Sarkar <sumeetsarkar4@gmail.com>
2018-11-25 23:29:26 +0530

    go json

---
+++ json.go
@@ -0,0 +1,61 @@
+package main
+
+import (
+	"encoding/json"
+	"fmt"
+)
+
+type Person struct {
+	Fname string
+	Lname string
+	Age   int
+	// Address	// will be treated as promoted field
+	/* Hence output will be
+		{
+	        "Fname": "Sumeet",
+	        "Lname": "Sarkar",
+	        "Age": 28,
+	        "street": "22nd Street",
+	        "city": "Bangalore",
+	        "pin": 560001
+	}
+	*/
+	Address Address
+	/* Address fields will be in Address block
+		{
+	        "Fname": "Sumeet",
+	        "Lname": "Sarkar",
+	        "Age": 28,
+	        "Address": {
+	                "street": "22nd Street",
+	                "city": "Bangalore",
+	                "pin": 560001
+	        }
+	}
+	*/
+}
+
+type Address struct {
+	Street  string `json:"street"` // custom json field naming
+	City    string `json:"city"`
+	Pincode int    `json:"pin"`
+}
+
+func main() {
+	person := &Person{
+		"Sumeet",
+		"Sarkar",
+		28,
+		Address{
+			"22nd Street",
+			"Bangalore",
+			560001,
+		},
+	}
+	// Note: small case fields, unexported fields will not be marshalled
+	b, err := json.MarshalIndent(person, "", "\t")
+	if err != nil {
+		panic("Error marshalling struct person")
+	}
+	fmt.Println(string(b))
+}
